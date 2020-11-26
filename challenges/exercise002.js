dcd3cbe261c0f75fbfa865c9614a775ece93d6a5
Dan Irani <dan.irani@fastdelta.com>
2020-11-26 15:42:01 +0000

    Exercise 2, sandwiches, Mancunians and bus tests completed successfully.

--- challenges/exercise002.js
+++ challenges/exercise002.js
@@ -1,16 +1,34 @@
 function getFillings(sandwich) {
   if (sandwich === undefined) throw new Error("ingredients is required");
-  // Your code here!
+
+  return sandwich.fillings;
 }
 
 function isFromManchester(person) {
   if (person === undefined) throw new Error("person is required");
-  // Your code here!
+
+  return person.city === "Manchester";
 }
 
 function getBusNumbers(people) {
   if (people === undefined) throw new Error("people is required");
-  // Your code here!
+
+  const busLimit = 40;
+  let fractionalCount = people / busLimit;
+  let integralCount = Math.floor(people / busLimit);
+
+  if (fractionalCount === integralCount) {
+    // everyone fits perfectly in a whole
+    // number buses with no spaces - super!
+
+    return integralCount;
+  }
+
+  // after fully filling `integralCount` busses,
+  // we still need an extra one for the remaining
+  // people - not good!
+
+  return integralCount + 1;
 }
 
 function countSheep(arr) {
