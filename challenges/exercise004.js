524317b567c2f5404d78bfc605f764cbd2c4f222
Dan Irani <dan.irani@fastdelta.com>
2020-11-27 09:54:13 +0000

    Exercise 4 challenges refactored using map and filter with all unit tests passing.

--- challenges/exercise004.js
+++ challenges/exercise004.js
@@ -1,110 +1,50 @@
 function findSmallNums(nums) {
   if (!nums) throw new Error("nums is required");
 
-  let smallArray = [];
-
-  nums.forEach(n => {
-    if (n < 1) {
-      smallArray.push(n);
-    }
-  });
-
-  return smallArray;
+  return nums.filter(value => value < 1);
 }
 
 function findNamesBeginningWith(names, char) {
   if (!names) throw new Error("names is required");
   if (!char) throw new Error("char is required");
 
-  let namesArray = [];
-
-  names.forEach(n => {
-    if (n[0] === char) {
-      namesArray.push(n);
-    }
-  });
-
-  return namesArray;
+  return names.filter(name => name[0] === char);
 }
 
 function findVerbs(words) {
   if (!words) throw new Error("words is required");
 
-  let verbsArray = [];
-
-  words.forEach(w => {
-    if (w.substr(0, 3) === "to ") {
-      verbsArray.push(w);
-    }
-  });
-
-  return verbsArray;
+  return words.filter(word => word.substr(0, 3) === "to ");
 }
 
 function getIntegers(nums) {
   if (!nums) throw new Error("nums is required");
 
-  let intArray = [];
-
-  nums.forEach(n => {
-    if (n === Math.trunc(n)) {
-      intArray.push(n);
-    }
-  });
-
-  return intArray;
+  return nums.filter(value => value === Math.trunc(value));
 }
 
 function getCities(users) {
   if (!users) throw new Error("users is required");
 
-  let cityArray = [];
-
-  users.forEach(c => {
-    cityArray.push(c.data.city.displayName);
-  });
-
-  return cityArray;
+  return users.map(user => user.data.city.displayName);
 }
 
 function getSquareRoots(nums) {
 
-  let rootsArray = [];
-
-  nums.forEach(n => {
-    if (n === Math.trunc(n)) {
-      rootsArray.push(Math.round(Math.sqrt(n) * 100.0) / 100);
-    }
-  });
-
-  return rootsArray;
+  return nums.map(n => Math.round(Math.sqrt(n) * 100.0) / 100);
 }
 
 function findSentencesContaining(sentences, str) {
   if (!sentences) throw new Error("sentences is required");
   if (!str) throw new Error("str is required");
 
-  let senArray = [];
-
-  sentences.forEach(s => {
-    if (s.toLowerCase().indexOf(str.toLowerCase()) !== -1) {
-      senArray.push(s);
-    }
-  });
-
-  return senArray;
+  return sentences.filter(line => line.toLowerCase().indexOf(str.toLowerCase()) !== -1);
 }
 
 function getLongestSides(triangles) {
   if (!triangles) throw new Error("triangles is required");
-
-  let longestArray = [];
-
-  triangles.forEach(t => {
-    longestArray.push(Math.max(...t));
-  });
-
-  return longestArray;
+  
+  return triangles.map(triples => Math.max(...triples));
 }
 
 module.exports = {
